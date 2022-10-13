37b8d671ef767f4c8123a839571a1a69b4a99286
Chris <113092264+Chriz923@users.noreply.github.com>
2022-10-13 08:46:47 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function display(value) {
+    document.getElementById("Result").value += value;
+}
+
+function calculate() {
+    const task = document.getElementById("Result").value;
+    const solution = eval(task);
+    document.getElementById("Result").value = solution;
+}
+
+function clearScreen(){
+    document.getElementById("Result").value = "";
+}
